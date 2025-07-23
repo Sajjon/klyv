189d471411082bf01b2d18230ecd5c13ace76a3d
Alexander Cyon <alex.cyon@gmail.com>
2025-07-23 10:46:41 +0200

    WIP

--- crates/core/src/logic/mod.rs
+++ crates/core/src/logic/mod.rs
@@ -1,11 +1,5 @@
 mod bridged;
-mod file_system_node;
-mod named_source_items;
-mod node_content;
 mod recursive_find_syn_items;
 
 pub use bridged::*;
-pub use file_system_node::*;
-pub use named_source_items::*;
-pub use node_content::*;
 pub use recursive_find_syn_items::*;
--- crates/core/src/models/mod.rs
+++ crates/core/src/models/mod.rs
@@ -1,5 +1,11 @@
 mod error;
+mod file_system_node;
+mod named_source_items;
+mod node_content;
 mod source_item;
 
 pub use error::*;
+pub use file_system_node::*;
+pub use named_source_items::*;
+pub use node_content::*;
 pub use source_item::*;
